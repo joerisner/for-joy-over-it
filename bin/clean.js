ca5e5221f18c1559fba944c8f075cbce9b13305e
joe <risnerjoseph@gmail.com>
2023-12-24 14:22:49 -0700

    Fix prettier errors

--- bin/clean.js
+++ bin/clean.js
@@ -2,10 +2,10 @@ import { rm } from 'fs';
 
 const removeList = ['./.astro/', './dist/'];
 
-const removeItem = (item) => {
-  rm(item, { force: true, recursive: true }, (error) => {
+const removeItem = item => {
+  rm(item, { force: true, recursive: true }, error => {
     if (error) console.error(error);
   });
 };
 
-removeList.forEach((item) => removeItem(item));
+removeList.forEach(item => removeItem(item));
