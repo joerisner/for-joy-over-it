470d3490d864c78c4cf4b1e6f0f8817d0f119511
joe <joe.risner@proton.me>
2024-02-02 23:39:30 -0700

    Bump Node and packages

---
+++ test/404.test.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('404 Page', () => {
+  test('renders custom page for 404 response', async ({ page }) => {
+    const mainContent = page.getByRole('main');
+    const backToHomeButton = page.getByRole('button');
+
+    await page.goto('/foo');
+
+    await expect(page).toHaveTitle('Not Found | Joseph Risner');
+    await expect(mainContent).toContainText('404');
+    await expect(mainContent).toContainText('Sorry, the page you are looking for does not exist.');
+    await expect(backToHomeButton).toHaveAttribute('href', '/');
+  });
+});
