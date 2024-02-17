59d5e858c29f966a9d4095284400670a637eae70
joe <joe.risner@proton.me>
2024-02-17 13:57:43 -0700

    Add test for random highlight

--- test/404.test.js
+++ test/404.test.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { SITE_TITLE } from 'src/config';
 
 test.describe('404 Page', () => {
   test('renders custom page for 404 response', async ({ page }) => {
@@ -7,7 +8,7 @@ test.describe('404 Page', () => {
 
     await page.goto('/foo');
 
-    await expect(page).toHaveTitle('Not Found | Joseph Risner');
+    await expect(page).toHaveTitle(`Not Found | ${SITE_TITLE}`);
     await expect(mainContent).toContainText('404');
     await expect(mainContent).toContainText('Sorry, the page you are looking for does not exist.');
     await expect(backToHomeButton).toHaveAttribute('href', '/');
---
+++ test/pages/highlights.test.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { SITE_TITLE } from 'src/config';
+
+test.describe('Highlights', () => {
+  test('renders random highlight', async ({ page }) => {
+    const authorName = page.locator('#author');
+    const sourceTitle = page.locator('#source');
+    const quoteText = page.getByRole('blockquote');
+
+    await page.route('**/random', async route => {
+      const json = { author: 'Terry Hogan', body: 'Ya brother!', source: 'Hulkamania' };
+      await route.fulfill({ json });
+    });
+
+    await page.goto('/highlights');
+    await expect(page).toHaveTitle(`Highlights | ${SITE_TITLE}`);
+    await expect(authorName).toHaveText('Terry Hogan');
+    await expect(sourceTitle).toHaveText('Hulkamania');
+    await expect(quoteText).toHaveText('Ya brother!');
+
+    await page.route('**/random', async route => {
+      const json = { author: 'Randy Savage', body: 'Ooh, yeah!', source: 'Macho Man' };
+      await route.fulfill({ json });
+    });
+
+    const fetchHighlightBtn = page.getByRole('button', { name: 'Enlighten Me, Boon' });
+    await fetchHighlightBtn.click();
+    await expect(authorName).toHaveText('Randy Savage');
+    await expect(sourceTitle).toHaveText('Macho Man');
+    await expect(quoteText).toHaveText('Ooh, yeah!');
+  });
+});
