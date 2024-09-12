f074d839fb1553f071138a57b0838e4abde0be75
joe <joe.risner@proton.me>
2024-09-12 16:54:00 -0600

    Add highlights modal

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,3 +1,9 @@
+export const kebabCase = (str: string) =>
+  str
+    .toLowerCase()
+    .replaceAll(/[^a-zA-Z ]/g, '')
+    .replaceAll(' ', '-');
+
 export const truncate = (str: string, length: number) => {
   if (str.length <= length) return str;
   return `${str.slice(0, length)}...`;
